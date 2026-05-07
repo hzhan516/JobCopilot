ab2fa18f6ff7b3c49c43b819cd296f2c040fb787
Guixingj <guixingj@asu.edu>
2026-05-07 00:28:51 -0700

    remove Database mover script

--- backend/app/src/main/resources/db/migration/V14__add_ai_optimized_version_id_to_conversations.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE conversations ADD COLUMN ai_optimized_version_id VARCHAR(64);
-CREATE INDEX IF NOT EXISTS idx_conversations_ai_optimized_version_id ON conversations(ai_optimized_version_id);
